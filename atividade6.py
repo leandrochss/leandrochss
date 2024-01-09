1a48455f854ecb501edcd06dab4f142ee22cb1f3
Leandro <142061828+leandrochss@users.noreply.github.com>
2024-01-09 19:34:40 -0300

    Delete atividade6.py

--- atividade6.py
+++
@@ -1,9 +0,0 @@
-# Atividade 6: Crie uma aplicação que solicite ao usuário digite o seu nome e sobrenomes e imprima no console o nome com todas as letras secretas e o sobrenome com todas as letras minúsculas sem espaços entre o nome e os sobrenomes e sem alterar a variável original.
-nome = input("Digite seu nome: ")
-sobrenome = input( "Digite seu sobrenome: ")
-
-nome_secreto = nome.upper()
-sobrenome_minusculo = sobrenome.replace(" ", "").lower()
-
-print("Nome secreto:", nome_secreto)
-print("Sobrenome em minúsculo:", sobrenome_minusculo)

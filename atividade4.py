8e7bb5dd48c875866ad469fb3fa588c2cf104ae0
Leandro <142061828+leandrochss@users.noreply.github.com>
2024-01-09 19:34:34 -0300

    Delete atividade4.py

--- atividade4.py
+++
@@ -1,4 +0,0 @@
-# Atividade 4: Crie uma aplicação que solicite ao usuário digite seu nome completo e imprima no console o nome com todas as letras guardadas.
-
-nome_completo = input("Digite seu nome completo:")
-print("Nome completo:", nome_completo)

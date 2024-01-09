f97ac2d9d781148ee560be44c74133b5a5bf74af
Leandro <142061828+leandrochss@users.noreply.github.com>
2024-01-09 19:34:28 -0300

    Delete atividade2.py

--- atividade2.py
+++
@@ -1,5 +0,0 @@
-# Atividade 2: Crie uma aplicação que solicite ao usuário digitar uma palavra e imprima no console a quandidade de caracteres que a palavra possui.
-
-palavra = input("Digite uma palavra: ")
-quantidade_carecteres = len(palavra)
-print("A palavra digitada possui", quantidade_carecteres, "caracteres.")
